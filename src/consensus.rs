71348dcd678fbac816acfc32948c2970e4aa202b
Roman Titov <ffuugoo@users.noreply.github.com>
2024-09-26 19:23:08 +0200

    Fix `collection_creation_passes_consensus` test

--- src/consensus.rs
+++ src/consensus.rs
@@ -1454,7 +1454,7 @@ mod tests {
             .unwrap();
 
         // Then
-        assert_eq!(consensus_state.hard_state().commit, 4); // Collection + 2 of shard activations
+        assert_eq!(consensus_state.hard_state().commit, 5); // first peer self-election + add first peer + create collection + activate shard x2
         assert_eq!(toc_arc.all_collections_sync(), vec!["test"]);
     }
 }
